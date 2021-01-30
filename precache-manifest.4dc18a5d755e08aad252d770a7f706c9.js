14cc9ceffcc34e0bc67007bc8c01a84d3b10f625
sajandhakal12 <sajan12dhakal@gmail.com>
2021-01-30 21:46:26 +0545

    Updates

---
+++ precache-manifest.4dc18a5d755e08aad252d770a7f706c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20bd1d23d6f343d4f7de10c816010ac7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "365182a03299dc3754ee",
+    "url": "./static/css/2.52db1f5a.chunk.css"
+  },
+  {
+    "revision": "194c7da085eded9a526a",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "365182a03299dc3754ee",
+    "url": "./static/js/2.0c07e987.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.0c07e987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "194c7da085eded9a526a",
+    "url": "./static/js/main.8262fe8e.chunk.js"
+  },
+  {
+    "revision": "c83327bdc8f2f5763056",
+    "url": "./static/js/runtime-main.4e6d8ee2.js"
+  }
+]);
